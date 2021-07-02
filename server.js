1607dad98ee8c8b78717eabcb6649eaf2d5965f4
sanjeevtalluri <sanjeevtalluri@gmail.com>
2021-07-02 11:38:16 +0530

    port added

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express=require('express');
 const app=express();
+const PORT=process.env.PORT||4444;
 app.get('/',(req,res)=>{
     res.send('Hello world');
 })
 
-app.listen(5555,()=>{
+app.listen(PORT,()=>{
     console.log('server started');
 })
